97b45edb5f223c893b1caa9654569e6e626b6342
wangfei44 <wangfei44@lenovo.com>
2022-01-26 14:51:46 +0800

    [draw_android]首次提交

---
+++ app/src/main/java/com/example/draw_android/section06_example/01_canvas_translate/CanvasTranslateView.kt
@@ -0,0 +1,16 @@
+package com.example.draw_android.section05_canvas
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+
+/**
+ * Created by wangfei44 on 2022/1/26.
+ */
+class CanvasTranslateView constructor(context: Context, attributeSet: AttributeSet) :
+    View(context, attributeSet) {
+    init {
+
+    }
+}
