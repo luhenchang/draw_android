03070c200f913f17646af85f28ad3a88f0106894
luhenchang <1276998208@qq.com>
2023-10-30 13:22:58 +0800

    画布曲线绘制-

--- app/src/main/java/com/example/draw_android/section05_canvas/e_curve/CurveView.kt
+++ app/src/main/java/com/example/draw_android/section05_canvas/e_curve/CurveView.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_DOWN
+import android.view.MotionEvent.ACTION_MOVE
 import android.view.View
 import kotlin.math.pow
 import kotlin.math.sqrt
@@ -13,6 +16,8 @@ import kotlin.math.sqrt
  */
 class CurveView constructor(context: Context, attributeSet: AttributeSet) :
     View(context, attributeSet) {
+
+    private lateinit var controlRect: Rect
 
     //网格的宽度
     private var mWidth = 0
@@ -28,9 +33,6 @@ class CurveView constructor(context: Context, attributeSet: AttributeSet) :
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        if (mWidth != w) {
-            sizeChange = true
-        }
         mWidth = w
         mHeight = h
         rect = RectF(0f, 0f, width.toFloat(), width.toFloat())
@@ -38,14 +40,14 @@ class CurveView constructor(context: Context, attributeSet: AttributeSet) :
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        if (sizeChange) {
-            Log.e("onDraw=","001")
-            BaseCanvasView.drawBaseBg(canvas, mWidth, mHeight)
-            sizeChange = false
-        }
+        Log.e("onDraw=", "001")
+        BaseCanvasView.drawBaseBg(canvas, mWidth, mHeight)
+        sizeChange = false
         drawLine(canvas)
         drawCircle(canvas)
+        drawQuz(canvas)
     }
+
     //圆方程式:(x-a)^2+(y-b)^2=r^2
     private fun drawCircle(canvas: Canvas) {
         val gPaint = Paint().apply {
@@ -74,6 +76,7 @@ class CurveView constructor(context: Context, attributeSet: AttributeSet) :
 
     private lateinit var pointList: ArrayList<PointF>
     private var number = -420..420
+
     //直线方程y=2x-80
     private fun drawLine(canvas: Canvas) {
         pointList = ArrayList()
@@ -93,5 +96,80 @@ class CurveView constructor(context: Context, attributeSet: AttributeSet) :
         }
     }
 
+    //记录移动的canvas画布坐标,不是手势坐标，由手势坐标转换为canvas坐标进行刷新
+    private var moveX: Float = 160f
+    private var moveY: Float = 160f
+    private fun drawQuz(canvas: Canvas) {
+        controlRect = Rect(
+            (moveX - 30f).toInt(),
+            (moveY + 30f).toInt(),
+            (moveX + 30).toInt(),
+            (moveY - 30f).toInt()
+        )
+        val quePath = Path()
+        canvas.drawCircle(0f, 0f, 10f, getPaintCir(Paint.Style.FILL))
+        canvas.drawCircle(320f, 0f, 10f, getPaintCir(Paint.Style.FILL))
+        //第一个点和控制点的连线到最后一个点链线。为了方便观察
+        val lineLeft = Path().apply {
+            moveTo(0f, 0f)
+            lineTo(moveX, moveY)
+            lineTo(320f, 0f)
+        }
+        canvas.drawPath(lineLeft, getPaint(Paint.Style.STROKE))
+        //第一个p0处画一个圆。第二个p1处画一个控制点圆,最后画一个。
+        canvas.drawCircle(moveX, moveY, 10f, getPaintCir(Paint.Style.FILL))
+        quePath.quadTo(moveX, moveY, 320f, 0f)
+        canvas.drawPath(quePath, getPaint(Paint.Style.STROKE))
+    }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            ACTION_DOWN,
+            ACTION_MOVE -> {
+                //在控制点附近范围内部,进行移动
+                Log.e(
+                    "x=",
+                    "onTouchEvent: (x,y)" + (event.x - width / 2).toInt() + ":" + (-(event.y - height / 2)).toInt()
+                )
+                //将手势坐标转换为屏幕坐标
+                moveX = event.x - width / 2
+                moveY = -(event.y - height / 2)
+                invalidate()
+            }
+        }
+        return true
+    }
+
+}
+
+private fun getPaint(style: Paint.Style): Paint {
+    val gPaint = Paint()
+    gPaint.color = Color.BLUE
+    gPaint.strokeWidth = 2f
+    gPaint.isAntiAlias = true
+    gPaint.style = style
+    gPaint.textSize = 26f
+    gPaint.color = Color.argb(255, 75, 151, 79)
+    return gPaint
+}
+
+private fun getPaint(style: Paint.Style, color: Int): Paint {
+    val gPaint = Paint()
+    gPaint.color = Color.BLUE
+    gPaint.strokeWidth = 2f
+    gPaint.isAntiAlias = true
+    gPaint.style = style
+    gPaint.textSize = 26f
+    gPaint.color = color
+    return gPaint
+}
+
+private fun getPaintCir(fill: Paint.Style): Paint {
+    val gPaint = Paint()
+    gPaint.strokeWidth = 12f
+    gPaint.isAntiAlias = true
+    gPaint.style = fill
+    gPaint.textSize = 26f
+    gPaint.color = Color.argb(255, 111, 111, 111)
+    return gPaint
 }
